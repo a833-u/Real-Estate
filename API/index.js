e5f725f387a788acb531d2dbdf389ac1c30eab4f
a833-u <akansara833@gmail.com>
2024-09-01 13:32:38 +0530

    user update API route created

--- API/index.js
+++ API/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './Routes/user.route.js';
-import authRouter from './Routes/auth.route.js';
+import userRouter from "./Routes/user.route.js";
+import authRouter from "./Routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -17,17 +18,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use('/api/user', userRouter);
+app.use("/api/user", userRouter);
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
